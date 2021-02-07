64a95141b7384bffbbd773c22c00eff2eecab274
KOmpiuter <mate.adamia.1@btu.edu.ge>
2021-02-07 18:59:31 +0400

    this is todo

---
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.ClipData;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.todo.Adapter.ToDoAdapter;
+import com.example.todo.Model.ToDoModel;
+import com.example.todo.Utils.DataBaseHelper;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnDialogCloseListner{
+    private RecyclerView myReclerview;
+    private FloatingActionButton fab;
+    private DataBaseHelper myDB;
+    private List<ToDoModel> mList;
+    private ToDoAdapter adapter;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        myReclerview= findViewById(R.id.recyclerview);
+        fab= findViewById(R.id.fab);
+        myDB = new DataBaseHelper(MainActivity.this);
+        mList = new ArrayList<>();
+        adapter = new ToDoAdapter(myDB,MainActivity.this);
+
+
+
+        myReclerview.setHasFixedSize(true);
+        myReclerview.setLayoutManager(new LinearLayoutManager(this ));
+        myReclerview.setAdapter(adapter);
+        mList = myDB.getAllTasks();
+        Collections.reverse(mList);
+        adapter.setTasks(mList);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddNewTask.newInstance().show(getSupportFragmentManager(), AddNewTask.TAG);
+
+            }
+
+
+            
+        });
+       ItemTouchHelper itemTouchHelper = new ItemTouchHelper( new RecyclerViewTouchHelper(adapter));
+        itemTouchHelper.attachToRecyclerView(myReclerview);
+    }
+
+    @Override
+    public void onDialogClose(DialogInterface dialogInterface) {
+        mList= myDB.getAllTasks();
+        Collections.reverse(mList);
+        adapter.setTasks(mList);
+        adapter.notifyDataSetChanged();
+
+    }
+}
